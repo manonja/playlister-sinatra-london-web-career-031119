ce87253944248d4e1ecc9ef69cf886327eb27d19
Manon Jacquin <manon.jacquin@gmail.com>
2019-04-05 09:33:43 +0100

    defines slug methods - works

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,14 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+
+  def slug
+    self.name.downcase.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(sample)
+    Artist.all.find {|artist| artist.slug == sample}
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(sample)
+    Song.all.find {|artist| artist.slug == sample}
+  end
 end
